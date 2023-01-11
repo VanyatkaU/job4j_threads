32cc52c153e467b05c85c91dc4d3508e34b3072b
Ivan Karelkin <ivanKa49@ya.ru>
2023-01-11 20:16:28 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -0,0 +1,36 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.function.Predicate;
+
+public class ParseFile {
+
+    private final File file;
+
+    public ParseFile(final File file) {
+        this.file = file;
+    }
+
+    public synchronized String content(Predicate<Character> filter) {
+        StringBuilder output = new StringBuilder();
+        try (BufferedInputStream i = new BufferedInputStream(new FileInputStream(file))) {
+            int data;
+            while ((data = i.read()) != 0) {
+                if (filter.test((char) data)) {
+                    output.append((char) data);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output.toString();
+    }
+
+    public synchronized String getContent() {
+        return content((data) -> true);
+    }
+
+    public synchronized String getContentWithoutUnicode() {
+        return content((data) -> data < 0x80);
+    }
+}
---
+++ src/main/java/ru/job4j/io/Write.java
@@ -0,0 +1,20 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Write {
+
+    private final File file;
+
+    public Write(final File file) {
+        this.file = file;
+    }
+
+    public synchronized void saveContent(String content) throws IOException {
+        try (BufferedOutputStream o = new BufferedOutputStream(new FileOutputStream(file))) {
+            for (int i = 0; i < content.length(); i += 1) {
+                o.write(content.charAt(i));
+            }
+        }
+    }
+}
