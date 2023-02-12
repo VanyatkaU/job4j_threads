b87d90e62b26cb3064fe226db176b56460289183
Ivan Karelkin <ivanKa49@ya.ru>
2023-02-12 18:36:42 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -0,0 +1,41 @@
+package ru.job4j.pool;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public final class EmailNotification {
+
+    private final ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors());
+
+    public void emailTo(User user) {
+        pool.submit(new Runnable() {
+            @Override
+            public void run() {
+                String subject = String.format("Notification {%s} to email {%s}.",
+                        user.getUsername(), user.getEmail());
+                String body = String.format("Add a new event to {%s}",
+                        user.getUsername());
+                send(subject, body, user.getEmail());
+            }
+        });
+    }
+
+
+    public void close() {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+}
