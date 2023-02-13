f27460ad1a275788aeb504205c40ea0885c410cf
Ivan Karelkin <ivanKa49@ya.ru>
2023-02-13 19:40:36 +0300

    0. CAS - операции [#6859 #386054]

--- src/test/Test/ru/job4j/CASCountTest.java
+++ src/test/Test/ru/job4j/CASCountTest.java
@@ -3,38 +3,65 @@ package ru.job4j;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CASCountTest {
 
     @Test
-    public void when3incrementThen3Get() {
+    public void when3incrementThen3Get() throws InterruptedException {
         CASCount casCount = new CASCount();
-        casCount.increment();
-        casCount.increment();
-        casCount.increment();
+        Thread thread = new Thread(() -> {
+               casCount.increment();
+               casCount.increment();
+        });
+        Thread thread1 = new Thread(() -> {
+            casCount.increment();
+        });
+        thread.start();
+        thread1.start();
+        Thread.sleep(3000);
         assertThat(casCount.get()).isEqualTo(3);
     }
 
     @Test
-    public void when1incrementThen1Get() {
+    public void when1incrementThen1Get() throws InterruptedException {
         CASCount casCount = new CASCount();
-        casCount.increment();
+        Thread thread = new Thread(() -> { });
+        Thread thread1 = new Thread(() -> {
+            casCount.increment();
+        });
+        thread.start();
+        thread1.start();
+        Thread.sleep(3000);
         assertThat(casCount.get()).isEqualTo(1);
     }
 
     @Test
-    public void when0incrementThen0Get() {
+    public void when0incrementThen0Get() throws InterruptedException {
         CASCount casCount = new CASCount();
+        Thread thread = new Thread(() -> { });
+        Thread thread1 = new Thread(() -> { });
+        thread.start();
+        thread1.start();
+        Thread.sleep(3000);
         assertThat(casCount.get()).isEqualTo(0);
     }
 
     @Test
-    public void when10incrementThen10Get() {
+    public void when10incrementThen10Get() throws InterruptedException {
         CASCount casCount = new CASCount();
-        for (int i = 0; i < 10; i++) {
-            casCount.increment();
-        }
+        Thread thread = new Thread(() -> {
+            for (int i = 0; i < 4; i++) {
+                casCount.increment();
+            }
+        });
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 6; i++) {
+                casCount.increment();
+            }
+        });
+        thread.start();
+        thread1.start();
+        Thread.sleep(3000);
         assertThat(casCount.get()).isEqualTo(10);
     }
 
